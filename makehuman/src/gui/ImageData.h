2918900fd7b71714c1f08bf3b0fd31fb4e24b125
Eli2 <FabianRoth64@googlemail.com>
2019-09-23 05:18:20 +0200

    Cleanup

--- makehuman/src/gui/ImageData.h
+++ makehuman/src/gui/ImageData.h
@@ -1,36 +1,8 @@
 /* SPDX-License-Identifier: GPL-3.0-or-later */
 /*
  *  Copyright (C) 2005-2007  MakeHuman Project
- *
- *  This program is free software; you  can  redistribute  it  and/or
- *  modify  it  under  the terms of the GNU General Public License as
- *  published by the Free Software Foundation; either  version  3  of
- *  the License, or (at your option) any later version.
- *
- *  This  program  is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the  implied  warranty  of
- *  MERCHANTABILITY  or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
- *  General Public License for more details.
- *
- *  You should have received a copy of the GNU General Public License
- *  along with this program; if not, write to the Free Software Foun-
- *  dation, Inc., 59 Temple Place, Suite 330, Boston,  MA  02111-1307
- *  USA
- *
- *  File   : ImageData.h
- *  Project: MakeHuman <info@makehuman.org>, http://www.makehuman.org/
- *  Library: MHGUI
- *
- *  For individual developers look into the AUTHORS file.
- *
  */
-
-#ifndef IMAGEDATA_H
-#define IMAGEDATA_H 1
-
-#ifdef HAVE_CONFIG_H
-#include <config.h>
-#endif
+#pragma once
 
 #include <stdint.h>
 #include <stdlib.h>
@@ -86,5 +58,3 @@ private:
 };
 
 } // namespace mhgui
-
-#endif // IMAGEDATA_H
