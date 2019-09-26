980b720e334362dd1541d03e0d1416fa66e1eace
Eli2 <FabianRoth64@googlemail.com>
2019-09-26 21:27:45 +0200

    Move material to shader

--- makehuman/src/gui/CGUtilities.cpp
+++ makehuman/src/gui/CGUtilities.cpp
@@ -369,15 +369,15 @@ int cgutils::initWindow(const Rect &rect, const char *title,
 	glEnable(GL_LIGHT1);
 
 	// Standard material
-	float mat_ambient[] = {0.7, 0.7, 0.7, 1.0};
-	float mat_diffuse[] = {0.8, 0.8, 0.8, 1.0};
-	float mat_specular[] = {1.0, 1.0, 1.0, 1.0};
-	float high_shininess[] = {100.0};
+	//float mat_ambient[] = {0.7, 0.7, 0.7, 1.0};
+	//float mat_diffuse[] = {0.8, 0.8, 0.8, 1.0};
+	//float mat_specular[] = {1.0, 1.0, 1.0, 1.0};
+	//float high_shininess[] = {100.0};
 
-	glMaterialfv(GL_FRONT, GL_AMBIENT, mat_ambient);
-	glMaterialfv(GL_FRONT, GL_DIFFUSE, mat_diffuse);
-	glMaterialfv(GL_FRONT, GL_SPECULAR, mat_specular);
-	glMaterialfv(GL_FRONT, GL_SHININESS, high_shininess);
+	//glMaterialfv(GL_FRONT, GL_AMBIENT, mat_ambient);
+	//glMaterialfv(GL_FRONT, GL_DIFFUSE, mat_diffuse);
+	//glMaterialfv(GL_FRONT, GL_SPECULAR, mat_specular);
+	//glMaterialfv(GL_FRONT, GL_SHININESS, high_shininess);
 
 	// Light0 features
 	float light_diffuse0[] = {light0Lum, light0Lum, light0Lum, light0Lum};
