648fffd5f035897d2e2073ac05093bdb90f3f370
Eli2 <FabianRoth64@googlemail.com>
2019-09-30 05:59:42 +0200

    morph target window work

--- makehuman/src/MhMorph.cpp
+++ makehuman/src/MhMorph.cpp
@@ -201,30 +201,248 @@ bool isCompositeMorphTarget(const std::string & target_name)
 void DisplayMorphTargets()
 {
 	
-	if(!ImGui::Begin("Morph Targets", &g_displayWin.morphTargets)) {
+//	if(!ImGui::Begin("Morph Targets", &g_displayWin.morphTargets)) {
+//		ImGui::End();
+//		return;
+//	}
+	
+//	BodySettings bodyset = g_global.mesh->getBodySettings();
+	
+//	for(const auto & targetEntry : g_global.mesh->getTargetMapRef()) {
+//		const string & target_name(targetEntry.first);
+		
+//		string::size_type loc = target_name.find("/", 0);
+//		if(loc == string::npos)
+//			continue;
+//		else {
+//			string sub = target_name.substr(0, loc);
+			
+//			if(isCompositeMorphTarget(target_name)) {
+//				continue;
+//			}
+			
+//			float target_value = bodyset[target_name];
+			
+//			DisplayMorphTargetRow(target_name, target_value, false);
+//		}
+//	}
+//	ImGui::End();
+
+	using glm::vec2;
+	using glm::ivec2;
+	
+	constexpr static ImGuiWindowFlags winFlags
+	    = ImGuiWindowFlags_NoScrollbar
+	    | ImGuiWindowFlags_NoResize;
+	
+	ImGui::SetNextWindowSize(ivec2(380, 484));
+	if(!ImGui::Begin("Morph Targets", &g_displayWin.morphTargets, winFlags)) {
 		ImGui::End();
 		return;
 	}
 	
-	BodySettings bodyset = g_global.mesh->getBodySettings();
+	static std::string category = "";
 	
-	for(const auto & targetEntry : g_global.mesh->getTargetMapRef()) {
-		const string & target_name(targetEntry.first);
+	
+	struct Tile {
 		
-		string::size_type loc = target_name.find("/", 0);
-		if(loc == string::npos)
-			continue;
-		else {
-			string sub = target_name.substr(0, loc);
+		void click() const {
+			Window & mainWindow = *g_mainWindow;
+			
+			PoseTargetPanel * targetPanel = dynamic_cast<PoseTargetPanel *>(
+				mainWindow.getPanel(kComponentID_TargetPanel));
+			
+			// Check if this Target Panel is not the current one?
+			if((targetPanel == NULL) || (targetPanel->getCategory() != target)) {
+				// No? The create it
+				int x = mainWindow.getSize().getWidth() - 210;
+				
+				mainWindow.removePanel(targetPanel);
+				delete targetPanel;
+				
+				targetPanel = new PoseTargetPanel(target, Rect(x, 40, 210, 517));
+				
+				mainWindow.addPanel(targetPanel);
+				targetPanel->createWidgets();
+			}
+			
+			targetPanel->show_all();
+		}
+		
+		
+		std::string as;
+		std::string tip;
+		std::string target;
+		std::optional<mh::Texture> tex;
+		std::optional<mh::Texture> texOver;
+		Tile(const std::string & img, const std::string & _tip, const std::string & _targ)
+		{
+			tip = _tip;
+			target = _targ;
+			as = "pixmaps/ui/" + img;
+			tex = LoadTextureFromFile(as + ".png");
+			texOver = LoadTextureFromFile(as + "_over.png");
+		}
+		
+		void gui() const {
+			//ImGui::SetColumnWidth(-1, 32);
+			
+			auto p = ImGui::GetCursorPos();
+			if(ImGui::InvisibleButton(as.c_str(), ivec2(32, 32))){
+				category = target;
+				click();
+			}
+			ImGui::SetCursorPos(p);
+			if(ImGui::IsItemHovered()) {
+				if(texOver)
+					MhGui::Image(texOver.value(), ivec2(32, 32));
+			} else {
+				if(tex)
+					MhGui::Image(tex.value(), ivec2(32, 32));
+			}
+			ImGui::NextColumn();
+		}
+	};
+
+	// clang-format off
+	static const auto goobar = {
+		Tile("body_01",  "Torso parameters",         "torso"),
+		Tile("body_02",  "Head parameters",          "head"),
+		Tile("face_01",  "X",                        ""),
+		Tile("face_02",  "X",                        ""),
+		Tile("face_03",  "X",                        ""),
+		Tile("face_04",  "X",                        ""),
+		// ==========================================
+		Tile("body_03",  "Shoulders parameters",     "shoulders"),
+		Tile("body_04",  "Neck parameters",          "neck"),
+		Tile("face_05",  "X",                        ""),
+		Tile("face_06",  "Forehead parameters",      "forehead"),
+		Tile("face_07",  "X",                        ""),
+		Tile("face_08",  "X",                        ""),
+		// ==========================================
+		Tile("body_05",  "Upper arms parameters",    "upper_arms"),
+		Tile("body_06",  "X",                        ""),
+		Tile("face_09",  "X",                        ""),
+		Tile("face_10",  "Eyes parameters",          "eyes"),
+		Tile("face_11",  "X",                        ""),
+		Tile("face_12",  "X",                        ""),
+		// ==========================================
+		Tile("body_07",  "Lower arms parameters",    "lower_arms"),
+		Tile("body_08",  "Abdomen parameters",       "abdomen"),
+		Tile("face_13",  "Nose parameters",          "nose"),
+		Tile("face_14",  "Cheek parameters",         "cheek"),
+		Tile("face_15",  "Ears parameters",          "ears"),
+		Tile("face_16",  "X",                        ""),
+		// ==========================================
+		Tile("body_09",  "Hands parameters",         "hands"),
+		Tile("body_10",  "Pelvis parameters",        "pelvis"),
+		Tile("face_17",  "Mouth parameters",         "mouth"),
+		Tile("face_18",  "X",                        ""),
+		Tile("face_19",  "X",                        ""),
+		Tile("face_20",  "X",                        ""),
+		// ==========================================
+		Tile("body_11",  "Upper legs parameters",    "upper_legs"),
+		Tile("body_12",  "X",                        ""),
+		Tile("face_21",  "Chin Jaw parameters",      "chin_jaw"),
+		Tile("face_22",  "X",                        ""),
+		Tile("face_23",  "X",                        ""),
+		Tile("face_24",  "X",                        ""),
+		// ==========================================
+		Tile("body_13",  "X",                        ""),
+		Tile("body_14",  "X",                        ""),
+		Tile("hands_01", "Forefinger parameters",    "forefinger"),
+		Tile("hands_02", "Middlefinger parameters",  "middlefinger"),
+		Tile("hands_03", "Ringfinger parameters",    "ringfinger"),
+		Tile("hands_04", "X",                        ""),
+		// ==========================================
+		Tile("body_15",  "Lower legs parameters",    "lower_legs"),
+		Tile("body_16",  "X",                        ""),
+		Tile("hands_05", "Pollex parameters",        "pollex"),
+		Tile("hands_06", "X",                        ""),
+		Tile("hands_07", "Littlefinger parameters",  "littlefinger"),
+		Tile("hands_08", "X",                        ""),
+		// ==========================================
+		Tile("body_17",  "Feet parameters",          "feet"),
+		Tile("body_18",  "X",                        ""),
+		Tile("hands_09", "X",                        ""),
+		Tile("hands_10", "X",     ""),
+		Tile("hands_11", "X",     ""),
+		Tile("hands_12", "X",     ""),
+		// ==========================================
+		Tile("teeth_01", "Tongue parameters",        "tongue"),
+		Tile("teeth_02", "Teeth parameters",         "teeth"),
+		Tile("teeth_03", "X",     ""),
+		Tile("teeth_04", "X",     ""),
+		Tile("teeth_05", "X",     ""),
+		Tile("teeth_06", "X",     ""),
+	};
+	// clang-format on
+	
+	//static_assert (goobar.size() == 84, "asd");
+	
+	{
+		ImGui::BeginChild("Pose Groups", ivec2(195, 460), false);
+		ImGui::PushStyleVar(ImGuiStyleVar_ItemSpacing, ivec2(0, 0));
+		ImGui::PushStyleVar(ImGuiStyleVar_ItemInnerSpacing, ivec2(0, 0));
+		ImGui::Columns(6, NULL, false);
+		for(const auto & tile: goobar) {
+			tile.gui();
+		}
+		ImGui::PopStyleVar(2);
+		ImGui::EndChild();
+	}
+	ImGui::SameLine();
+	{
+		ImGui::BeginChild("Pose Targets", ivec2(140, 440), false);
+		
+		BodySettings bodyset = g_global.mesh->getPoses();
+		
+		
+		
+		for(const auto & targetEntry : g_global.mesh->getTargetMapRef()) {
+			const string & target_name(targetEntry.first);
+	
+			string::size_type loc = target_name.find("/", 0);
+			if(loc == string::npos)
+				continue;
 			
 			if(isCompositeMorphTarget(target_name)) {
 				continue;
 			}
 			
+			string sub = target_name.substr(0, loc);
+
+
+
 			float target_value = bodyset[target_name];
 			
-			DisplayMorphTargetRow(target_name, target_value, false);
+			
+			
+			if(sub != category)
+				continue;
+			
+			//ImGuiIO& io = ImGui::GetIO();
+			
+			
+			fs::path targetImageName = target_name;
+			targetImageName.replace_extension();
+			
+			const auto & texIdIt = g_targetImageTextures.find(targetImageName);
+			if(texIdIt != g_targetImageTextures.end()) {
+				auto texId = texIdIt->second;
+				MhGui::ImageButton(texId, ImVec2(64, 64));
+			} else {
+				ImGui::Text(target_name.c_str());
+				//ImGui::InvisibleButton(target_name.c_str(), ImVec2(48, 48));
+			}
+			
+			//DisplayMorphTargetRow(target_name, target_value, false);
 		}
+		
+		
+		
+		
+		ImGui::EndChild();
 	}
 	ImGui::End();
 }
