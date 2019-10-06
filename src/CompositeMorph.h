67e16ec78e173e82e65ce02ef20add4b8e1b4900
Eli2 <FabianRoth64@googlemail.com>
2019-10-06 05:05:26 +0200

    move grid size to template parameters

--- src/CompositeMorph.h
+++ src/CompositeMorph.h
@@ -4,19 +4,58 @@
 #include <glm/glm.hpp>
 #include "animorph/BodySettings.h"
 
+template<int rows, int cols>
 struct Grid
 {
-	int rows = 2;
-	int cols = 2;
-	
 	float maxValue;
 	float cellRatio;
 	
 	// from left-bottom point to right-up point
 	std::vector<glm::ivec2> points;
 	
-	void calcPoints(glm::ivec2 size, int inRows, int inCols);
-	std::vector<float> calculateDists(glm::ivec2 cursorPos) const;
+	//void calcPoints(glm::ivec2 size, int inRows, int inCols);
+	
+	void calcPoints(glm::ivec2 size)
+	{
+		points.clear();
+		
+		for(int i = 0; i < rows; i++) {
+			for(int j = 0; j < cols; j++) {
+				glm::ivec2 tmp(j * size.x / (cols - 1),
+				               i * size.y / (rows - 1));
+				points.push_back(tmp);
+			}
+		}
+		
+		float cellWidth  = size.x / (cols - 1);
+		float cellHeight = size.y / (rows - 1);
+		
+		cellRatio = cellWidth / cellHeight;
+		maxValue  = glm::min(cellWidth, cellHeight * cellRatio);
+	}
+	
+	std::vector<float> calculateDists(glm::ivec2 cursorPos) const
+	{
+		std::vector<float> ret;
+		
+		for(const auto & vp_it : points) {
+			const glm::ivec2 & tmp(vp_it);
+			
+			float dist  = sqrt(pow(tmp.x - cursorPos.x, 2) +
+                              pow((tmp.y - cursorPos.y) * cellRatio, 2));
+			float value = 1 - (dist / maxValue);
+			if(value > 0) {
+				ret.push_back(value);
+			} else {
+				ret.push_back(0.0f);
+			}
+		}
+		
+		return ret;
+	}
+	
+	
+	//std::vector<float> calculateDists(glm::ivec2 cursorPos) const;
 };
 
 struct Grids {
@@ -25,10 +64,10 @@ struct Grids {
 	glm::ivec2 breastPos;
 	glm::ivec2 shapePos;
 	
-	Grid ageGrid;
-	Grid muscleSizeGrid;
-	Grid breastGrid;
-	Grid shapeGrid;
+	Grid<2, 5> ageGrid;
+	Grid<2, 2> muscleSizeGrid;
+	Grid<2, 2> breastGrid;
+	Grid<2, 2> shapeGrid;
 	
 	std::vector<float> ageDists;
 	std::vector<float> muscleSizeDists;
@@ -37,10 +76,10 @@ struct Grids {
 	
 	Grids()
 	{
-		ageGrid.calcPoints(glm::ivec2(192, 104), 2, 5);
-		muscleSizeGrid.calcPoints(glm::ivec2(192, 104), 2, 2);
-		breastGrid.calcPoints(glm::ivec2(192, 104), 2, 2);
-		shapeGrid.calcPoints(glm::ivec2(192, 104), 2, 2);
+		ageGrid.calcPoints(glm::ivec2(192, 104));
+		muscleSizeGrid.calcPoints(glm::ivec2(192, 104));
+		breastGrid.calcPoints(glm::ivec2(192, 104));
+		shapeGrid.calcPoints(glm::ivec2(192, 104));
 	}
 	
 	void calcDists();
