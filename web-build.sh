0d71bb8a2d4c8e246998bde3e53430ba02f788a8
Eli2 <FabianRoth64@googlemail.com>
2025-05-03 10:12:13 +0200

    cmake cleanup

--- web-build.sh
+++ web-build.sh
@@ -1,11 +1,4 @@
 #!/bin/bash
 rm -r build-web
 EM_CONFIG=$HOME/.emscripten
-emcmake cmake \
--S . \
--B build-web \
--D MH_USE_GLES=ON \
--D MH_USE_SYSTEM_FMT=OFF \
--D MH_USE_SYSTEM_SDL=OFF \
--D MH_USE_SYSTEM_GLM=OFF \
--D MH_USE_SYSTEM_PHYSFS=OFF
+emcmake cmake -S . -B build-web
